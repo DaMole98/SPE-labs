b43bce8ff2f0290249c64bf0d83db398630619ae
Congiuntivo <simonedecarli19@gmail.com>
2025-05-27 16:58:54 +0200

    fmt

--- HW-4/src/scheduler.py
+++ HW-4/src/scheduler.py
@@ -1,12 +1,14 @@
-
 import heapq
+
 
 class Scheduler:
     _instance = None
 
     def __init__(self):
         if Scheduler._instance is not None:
-            raise Exception("Use the 'init()' method to get the instance of this class.")
+            raise Exception(
+                "Use the 'init()' method to get the instance of this class."
+            )
 
     @classmethod
     def init(cls):
@@ -15,17 +17,18 @@ class Scheduler:
             # Initialize instance variables here
             cls._instance.event_queue = []
         return cls._instance
-    
 
-    def schedule(self,event):
+    def schedule(self, event):
         """
         Schedule an event.
         """
-        heapq.heappush(self.event_queue, event) #heapq implements a min-heap, so the smallest
-                                                                    #(accordingly to the overloaded "<" in the Event class") 
-                                                                    # event will be at the root
+        heapq.heappush(
+            self.event_queue, event
+        )  # heapq implements a min-heap, so the smallest
+        # (accordingly to the overloaded "<" in the Event class")
+        # event will be at the root
         return event.time
-    
+
     def get_next_event(self):
         """
         Get the next event from the queue.
@@ -34,19 +37,19 @@ class Scheduler:
             return heapq.heappop(self.event_queue)
         else:
             return None
-    
+
     def is_empty(self):
         """
         Check if the event queue is empty.
         """
         return len(self.event_queue) == 0
-    
+
     def get_queue_length(self):
         """
         Get the length of the event queue.
         """
         return len(self.event_queue)
-    
+
     def flush(self):
         """
         Flush the event queue.
